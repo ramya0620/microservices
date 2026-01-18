57e59b065ca853e05fc90ebce6d6cbdfcf213240
Ramya <ramyapentela0620@gmail.com>
2026-01-18 12:24:02 -0500

    Added request validation for AccountDTO and CustomerDTO with @NotEmpty @Email @Pattern Enabled controller-level validation and enhanced global exception handling to return validation errors to clients

--- src/main/java/com/vedget/accounts/dto/AccountsDto.java
+++ src/main/java/com/vedget/accounts/dto/AccountsDto.java
@@ -1,12 +1,18 @@
 package com.vedget.accounts.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "accountNumber can not be empty or null")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "account number must be 10 digits ")
     private Long accountNumber;
+   @NotEmpty(message = "accountType cannot be empty or null")
     private String accountType;
+   @NotEmpty(message = "branchAddress can not be empty or null")
     private String branchAddress;
 }
--- src/main/java/com/vedget/accounts/dto/CustomerDto.java
+++ src/main/java/com/vedget/accounts/dto/CustomerDto.java
@@ -1,14 +1,25 @@
 package com.vedget.accounts.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message="name can not be empty or null")
+    @Size(min=3,max=30,message="length of the name should be between 3 and 30")
     public String name;
+    @NotEmpty(message="email can not be empty or null")
+    @Email(message = "email address should be valid value")
     public String email;
+
+    @Pattern(regexp="(^$|[0-9]{10})",message="mobile number should be 10 digits")
     public String mobileNumber;
+
     public AccountsDto accountsDto;
 }
