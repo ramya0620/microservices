5ac509cc4dca531a993c85c1f01723230cc9c5df
Ramya <ramyapentela0620@gmail.com>
2026-01-18 12:52:37 -0500

    Implemented JPA auditing for BaseEntity fields Auto-populated created/modified dates and user via AuditorAware and removed manual audit handling from create API

--- src/main/java/com/vedget/accounts/entity/BaseEntity.java
+++ src/main/java/com/vedget/accounts/entity/BaseEntity.java
@@ -1,21 +1,36 @@
 package com.vedget.accounts.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+
 @MappedSuperclass//this class will act as superclass for all the entities
+@EntityListeners(AuditingEntityListener.class)
 @Getter @Setter @ToString
 public class BaseEntity {
+    @CreatedDate
     @Column(updatable = false) //jpa don't have to update/create just for the time when it is updated
     private LocalDateTime createdAt;
+
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
+
+    @LastModifiedDate
     @Column(updatable = false)
     private LocalDateTime updatedAt;
+
+    @LastModifiedBy
     @Column(updatable = false)
     private String updatedBy;
 }
