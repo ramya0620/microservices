8c29b23c89592c6a4381b49e803ee7293ca22770
Ramya <ramyapentela0620@gmail.com>
2026-01-21 11:50:33 -0500

    Fix cards microservice structure: add src, pom, and remove target folder

---
+++ cards/src/main/java/com/vedget/cards/CardsApplication.java
@@ -0,0 +1,43 @@
+package com.vedget.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/*@ComponentScans({ @ComponentScan("com.vedget.cards.controller") })
+@EnableJpaRepositories("com.vedget.cards.repository")
+@EntityScan("com.vedget.cards.model")*/
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "VedgetBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Ramya P",
+						email = "ramyap0620@gmail.com",
+						url = "https://www.vedget.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.vedget.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "VedgetBank Cards microservice REST API Documentation",
+				url = "https://www.Vedget.com/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
