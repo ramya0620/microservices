f92e71e4b21238019cbabc99ea5a493d9a2331b2
Ramya <ramyapentela0620@gmail.com>
2026-01-25 11:13:08 -0500

    application.yml properties were set in the contactDto

--- loans/src/main/java/com/vedget/loans/dto/LoansContactDto.java
+++ loans/src/main/java/com/vedget/loans/dto/LoansContactDto.java
@@ -1,10 +1,10 @@
-package com.vedget.accounts.dto;
+package com.vedget.loans.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts")
-public record AccountContactDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@ConfigurationProperties(prefix = "loans")
+public record LoansContactDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
 }
